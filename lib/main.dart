962185931d43c4dd3c26571abfc3cf6442fa21df
Danie <danielalejandro347@gmail.com>
2024-09-25 09:57:32 -0300

    Using generic_bloc_provider package to expose bloc functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trip_app/User/bloc/bloc_user.dart';
 import 'package:platzi_trip_app/platzi_trips.dart';
 //import 'package:platzi_trip_app/widgets/platzi_trips_cupertino.dart';
 
@@ -12,41 +14,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
-      home: const PlatziTrips(),
-      /* home: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              "My Test Widget",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontFamily: "Lato",
-                  fontWeight: FontWeight.w500),
-            ),
-            backgroundColor: Colors.black87,
-            actions: const <Widget>[
-              Icon(
-                Icons.search,
-                color: Colors.white70,
-              ),
-              Icon(
-                Icons.camera,
-                color: Colors.white70,
-              ),
-              Icon(
-                Icons.person,
-                color: Colors.white70,
-              )
-            ],
-          ),
-          body: const MyTestWidget()), */
-    );
+    return BlocProvider(
+        bloc: UserBloc(),
+        child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          home: PlatziTrips(),
+        ));
   }
 }
