6ebdfe2bb9987bdf57e997540a05a20d3c99a02c
Danie <danielalejandro347@gmail.com>
2024-09-26 14:27:39 -0300

    After a successful login with a Google account, redirect to the Home Page.

--- lib/User/bloc/bloc_user.dart
+++ lib/User/bloc/bloc_user.dart
@@ -5,6 +5,12 @@ import 'package:platzi_trip_app/User/repository/auth_repository.dart';
 class UserBloc implements Bloc {
   final authRepository = AuthRepository();
 
+  // FLujo de datos Streams: Firebase ya viene con métodos que nos devuelven
+  // el flujo de datos y nosotros decidimos que hacer con ese stream.
+  // Si no tuvieramos ese flujo de datos utilizariamos la clase StreamController.
+  Stream<User?> streamFirebase = FirebaseAuth.instance.authStateChanges();
+  Stream<User?> get authStatus => streamFirebase;
+
   // 1er Caso de uso de la clase User: SignIn a la aplicación Google.
   Future<User?> signIn() {
     return authRepository.signInFirebase();
--- lib/User/ui/screens/sign_in_screen.dart
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:platzi_trip_app/User/bloc/bloc_user.dart';
+import 'package:platzi_trip_app/platzi_trips_cupertino.dart';
 import 'package:platzi_trip_app/widgets/button_green.dart';
 import 'package:platzi_trip_app/widgets/gradient_back.dart';
 
@@ -18,7 +19,20 @@ class _SignInScreenState extends State<SignInScreen> {
   @override
   Widget build(BuildContext context) {
     userBloc = BlocProvider.of(context);
-    return signInGoogleUI();
+    return _handleCurrentSession();
+  }
+
+  Widget _handleCurrentSession() {
+    return StreamBuilder(
+        stream: userBloc.authStatus,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          // snapshot - data - Object User
+          if (!snapshot.hasData || snapshot.hasError) {
+            return signInGoogleUI();
+          } else {
+            return const PlatziTripsCupertino();
+          }
+        });
   }
 
   Widget signInGoogleUI() {
